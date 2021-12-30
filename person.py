d5bfb4a34d3ebee4248311bab1bf67c570d447b7
harrywatkinson <harry@revmo.app>
2021-12-30 20:25:37 +0000

    minor refactor

--- person.py
+++ person.py
@@ -6,12 +6,16 @@ NAMES_FP = path.abspath("names.txt")
 
 
 class Person:
+
+    def __init__(self):
+        self.uuid = uuid4()
+        self.name = self.random_name(NAMES_FP)
+
     @staticmethod
     def random_name(file_path: path):
         names = open(file_path).read().splitlines()
         name = choice(names)
         return name
 
-    def __init__(self):
-        self.uuid = uuid4()
-        self.name = self.random_name(NAMES_FP)
+    def __repr__(self):
+        return f'person: {self.name}'
