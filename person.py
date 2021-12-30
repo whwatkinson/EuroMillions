d078e7f5b2fd7a35078e277a69164770e74d7afa
harrywatkinson <harry@revmo.app>
2021-12-30 20:23:56 +0000

    black

--- person.py
+++ person.py
@@ -5,13 +5,13 @@ from uuid import uuid4
 NAMES_FP = path.abspath("names.txt")
 
 
-def random_name(file_path: path):
-    names = open(file_path).read().splitlines()
-    name = choice(names)
-    return name
-
-
 class Person:
+    @staticmethod
+    def random_name(file_path: path):
+        names = open(file_path).read().splitlines()
+        name = choice(names)
+        return name
+
     def __init__(self):
         self.uuid = uuid4()
-        self.name = self.random_name
+        self.name = self.random_name(NAMES_FP)
