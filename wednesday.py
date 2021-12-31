8fe6be52846b555603858d0113b456a8be07beab
harrywatkinson <harry@revmo.app>
2021-12-31 10:41:52 +0000

    added wendesday

---
+++ wednesday.py
@@ -0,0 +1,12 @@
+from draw import Draw
+from lucky_dip_ticket import LuckDipTicket, LuckDipTicketList
+
+
+class Wednesday:
+
+    def __init__(self, draw: Draw, ticket: LuckDipTicket):
+        self.draw = draw
+        self.ticket = ticket
+
+    def check_results(self):
+        pass
